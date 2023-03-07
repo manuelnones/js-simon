1dab27ae34ceeea9ccc84a6a4051eff9ee08f3d4
manuelnones <manuelnones99@yahoo.com>
2023-03-07 20:24:55 +0100

    fin exercise

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@ Dopo 10 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
-    - creare un array di 5 numeri 
-    - creare un setInterval di 10 secondi
-    - se timer è minore o uguale a 0 
+    x creare un array di 5 numeri 
+    x creare un setInterval di 10 secondi
+    x se timer è minore o uguale a 0 
         o fermare il setInterval
         o l' array di numeri scompare
-    - creare un prompt che chiede l' array di numeri visualizzati precedentemente
+    x creare un prompt che chiede l' array di numeri visualizzati precedentemente
+    - se i numeri inseriti nel prompt coincidono con quelli visualizzati precedentemente
+        o dire all' utente  che ha inserito i numeri correttamente 
+    ALTRIMENTI 
+        o dire all' utente quanti numeri erano stati inseriti correttamente  e quali erano quelli sbagliati
 */
 
 let numbers = arrayNumbersGenerator(5, 100);
 let timer = 10;
+let userNumbers = [];
+
 
 document.writeln(numbers);
 let time = setInterval(contDown, 1000);
+setTimeout(isEqual, 11500);
 
-if (timer <= 0) {
-    stopContDown();
-    numbers = [``];
-}
+
 
 
 
@@ -29,7 +33,7 @@ if (timer <= 0) {
 
 function randomNumberGenerator(lastNumber) {
     let randomNumber = Math.floor(Math.random() * lastNumber + 1);
-    
+
     return randomNumber;
 };
 
@@ -39,10 +43,10 @@ function arrayNumbersGenerator(maxNumber, lastNumber) {
 
     while (arrayNumbers.length < maxNumber) {
         let randomNumbers = randomNumberGenerator(lastNumber);
-        
+
         if (!arrayNumbers.includes(randomNumbers)) {
             arrayNumbers.push(randomNumbers);
-            
+
         };
 
     };
@@ -51,14 +55,62 @@ function arrayNumbersGenerator(maxNumber, lastNumber) {
 };
 
 
-function contDown () {
+function contDown() {
     console.log(timer);
     timer--;
+
+
+    if (timer < 0) {
+        document.writeln(``);
+        stopContDown();
+
+    }
 
 };
 
 
-function stopContDown () {
+function stopContDown() {
     clearInterval(time);
 
-}
+};
+
+
+function createPrompt() {
+    for (i = 1; i <= 5; i++) {
+        let userNumber = parseInt(prompt(`Inserisci i numeri visti precedentemente`));
+
+        userNumbers.push(userNumber);
+
+    };
+
+    return userNumbers;
+
+};
+
+
+function isEqual() {
+    createPrompt();
+
+    if (userNumbers.toString() === numbers.toString()) {
+        document.writeln(`Hai inserito tutti i numeri correttamente`);
+
+    } else if (userNumbers[0] != numbers[0]) {
+        document.writeln(`Il numero ` + userNumbers[0] + ` non è corretto`);
+
+    } else if (userNumbers[1] != numbers[1]) {
+        document.writeln(`Il numero ` + userNumbers[1] + ` non è corretto`);
+
+    } else if (userNumbers[2] != numbers[2]) {
+        document.writeln(`Il numero ` + userNumbers[2] + ` non è corretto`);
+
+    } else if (userNumbers[3] != numbers[3]) {
+        document.writeln(`Il numero ` + userNumbers[3] + ` non è corretto`);
+
+    } else if (userNumbers[4] != numbers[4]) {
+        document.writeln(`Il numero ` + userNumbers[4] + ` non è corretto`);
+
+    };
+
+    console.log(userNumbers);
+
+};
